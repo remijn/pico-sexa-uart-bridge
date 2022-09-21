cfa4b8128f5dd135e71613eccff6c3d7a7949826
Joe Schaack <JoeSc@users.noreply.github.com>
2022-09-21 15:12:55 -0500

    Update build script to follow conventions

--- build.sh
+++ build.sh
@@ -4,18 +4,14 @@ BUILD_DIR=build
 PICO_SDK_DIR=pico-sdk
 
 main () {
-	local cur_dir=$PWD
-
-	if [ ! -d "$PICO_SDK_DIR/.git" ]; then
+	if [ ! -f "$PICO_SDK_DIR/.git" ]; then
+		git submodule sync --recursive
 		git submodule update --init --recursive
 	fi
 
 	mkdir -p $BUILD_DIR
-	cd $BUILD_DIR
-	cmake ../
-	make
-
-	cd $cur_dir
+	cmake -B $BUILD_DIR
+	make -C $BUILD_DIR
 }
 
 main $@
